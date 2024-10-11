2c2dc6f1d64134d2fc5b23cb538c11fee02f4c9a
Angelo <angelobracero35@gmail.com>
2024-10-11 17:16:46 +0800

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        richblue: "#020D19",
+        lightgreen: "#81E979",
+        lightblue: "#1C6E8C",
+      },
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
